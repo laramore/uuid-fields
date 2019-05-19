edafff413ec351c658680493285d99353df4fffc
NastuzziSamy <samy@nastuzzi.fr>
2019-05-19 18:40:12 +0200

    Add a PrimaryUuid field
    Signed-off-by: NastuzziSamy <samy@nastuzzi.fr>

---
+++ src/Fields/PrimaryUuid.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Define an uuid field.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2019
+ * @license MIT
+ */
+
+namespace Laramore\Fields;
+
+use Laramore\Interfaces\IsAPrimaryField;
+
+class PrimaryUuid extends Uuid implements IsAPrimaryField
+{
+    protected $generateByDefault = true;
+}
